a5a0da2eb46df80a0c77854e5b75fa10b50ea3c7
王帅琪 <961345333@qq.com>
2018-03-16 22:06:58 +0800

    add dict model

--- ip.py
+++ ip.py
@@ -1,14 +1,15 @@
 import os
 from model import Dict
 import pymongo
-client = pymongo.MongoClient("mongodb://m.mywsq.cn:27017")
-data_list = open(os.path.join(os.path.dirname(__file__), 'data', 'neg_all_dict.txt'))
+
+client = pymongo.MongoClient("mongodb://mywsq.cn:27017")
+col = client['test'].get_collection('dict')
+data_list = open(os.path.join(os.path.dirname(__file__), 'data', 'stop_words.txt'))
 
 if __name__ == '__main__':
-    count = len(data_list.readline())
-    tmp = 0
+    tmp = []
     for each in data_list:
-        Dict(word=each, type='pos').save()
-        tmp += 1
-        print(float(tmp) / count)
+        tmp.append({'word': each.rstrip(), 'type': 'pos'})
 
+
+#
